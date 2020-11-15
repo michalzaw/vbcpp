c2b3e06f1858a67d0ac14cf33835339afeeacda5
michalzaw <mich_z@op.pl>
2020-11-15 20:12:48 +0100

    Removed unused in Bullet flags from PhysicsDebuggerWindow.

--- ImGuiInterface/PhysicsDebuggerWindow.cpp
+++ ImGuiInterface/PhysicsDebuggerWindow.cpp
@@ -45,18 +45,10 @@ void PhysicsDebuggerWindow::drawWindow()
 		drawDebugModeCheckBox(debugMode, "DBG_NoDebug", btIDebugDraw::DBG_NoDebug);
 		drawDebugModeCheckBox(debugMode, "DBG_DrawWireframe", btIDebugDraw::DBG_DrawWireframe);
 		drawDebugModeCheckBox(debugMode, "DBG_DrawAabb", btIDebugDraw::DBG_DrawAabb);
-		drawDebugModeCheckBox(debugMode, "DBG_DrawFeaturesText", btIDebugDraw::DBG_DrawFeaturesText);
 		drawDebugModeCheckBox(debugMode, "DBG_DrawContactPoints", btIDebugDraw::DBG_DrawContactPoints);
 		drawDebugModeCheckBox(debugMode, "DBG_NoDeactivation", btIDebugDraw::DBG_NoDeactivation);
-		drawDebugModeCheckBox(debugMode, "DBG_NoHelpText", btIDebugDraw::DBG_NoHelpText);
-		drawDebugModeCheckBox(debugMode, "DBG_DrawText", btIDebugDraw::DBG_DrawText);
-		drawDebugModeCheckBox(debugMode, "DBG_ProfileTimings", btIDebugDraw::DBG_ProfileTimings);
-		drawDebugModeCheckBox(debugMode, "DBG_EnableSatComparison", btIDebugDraw::DBG_EnableSatComparison);
-		drawDebugModeCheckBox(debugMode, "DBG_DisableBulletLCP", btIDebugDraw::DBG_DisableBulletLCP);
-		drawDebugModeCheckBox(debugMode, "DBG_EnableCCD", btIDebugDraw::DBG_EnableCCD);
 		drawDebugModeCheckBox(debugMode, "DBG_DrawConstraints", btIDebugDraw::DBG_DrawConstraints);
 		drawDebugModeCheckBox(debugMode, "DBG_DrawConstraintLimits", btIDebugDraw::DBG_DrawConstraintLimits);
-		drawDebugModeCheckBox(debugMode, "DBG_FastWireframe", btIDebugDraw::DBG_FastWireframe);
 		drawDebugModeCheckBox(debugMode, "DBG_DrawNormals", btIDebugDraw::DBG_DrawNormals);
 		drawDebugModeCheckBox(debugMode, "DBG_DrawFrames", btIDebugDraw::DBG_DrawFrames);
 
