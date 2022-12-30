8b824bc0a9039e7d3fac6a940892a45c6275c59c
michalzaw <mich_z@op.pl>
2022-12-30 15:13:40 +0100

    Improved test window with result of compute shader.

--- Editor/Windows/ComputeShaderTestWindow.cpp
+++ Editor/Windows/ComputeShaderTestWindow.cpp
@@ -6,7 +6,7 @@ ComputeShaderTestWindow::ComputeShaderTestWindow(bool isOpen/* = false*/)
 {
 	_computeShader = ResourceManager::getInstance().loadComputeShader("Shaders/computeShader.comp");
 
-	_texture = new RTexture2D("aaaa", (float*) nullptr, TF_RGBA_32F, glm::uvec2(512, 512), false);
+	_texture = new RTexture2D("ComputeShaderResult", (float*) nullptr, TF_RGBA_32F, glm::uvec2(1000, 1000), false);
 	_texture->setClampMode(TCM_CLAMP_TO_EDGE);
 	_texture->setFiltering(TFM_LINEAR, TFM_LINEAR);
 }
@@ -16,11 +16,21 @@ void ComputeShaderTestWindow::drawWindow()
 {
 	if (ImGui::Begin("Compute shader test", &_isOpen))
 	{
-		if (ImGui::Button("Run compute shader"))
+		static float t = 0;
+		ImGui::DragFloat("Time", &t, 0.01f, 0.0f, 100.0f);
+
+		static glm::ivec3 numGroups = glm::ivec3(_texture->getSize().x / 10, _texture->getSize().y / 10, 1);
+		ImGui::DragInt3("Number of groups", &numGroups[0]);
+
+		static bool runComputeShader = false;
+		ImGui::Checkbox("Run compute shader (loop)", &runComputeShader);
+
+		if (ImGui::Button("Run compute shader") || runComputeShader)
 		{
 			_computeShader->enable();
 			_computeShader->bindTextureImage(_texture, 0, 0, TIBM_READ_AND_WRITE);
-			_computeShader->runComputeShader(512, 512, 1);
+			_computeShader->setUniform(_computeShader->getUniformLocation("t"), t);
+			_computeShader->runComputeShader(numGroups.x, numGroups.y, numGroups.z);
 		}
 
 		ImGui::Image((ImTextureID) _texture->getID(), ImVec2(_texture->getSize().x, _texture->getSize().y));
