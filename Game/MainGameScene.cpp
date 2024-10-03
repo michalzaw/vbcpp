3dc86039a3f85ffa0f09f225d8afc0f81af3d952
michalzaw <mich_z@op.pl>
2024-10-03 21:56:07 +0200

    Small fixes in Camera Control Component.

--- Game/MainGameScene.cpp
+++ Game/MainGameScene.cpp
@@ -207,6 +207,8 @@ void MainGameScene::setActiveCamera(CameraFPS* camera)
 			}
 		}
 	}
+
+	setCameraControll(_isCameraControll);
 }
 
 
@@ -417,7 +419,7 @@ void MainGameScene::startGame()
 	_physicsManager->play();
 	_soundManager->setMute(false);
 
-	glfwSetCursorPos(_window->getWindow(), _window->getWidth() / 2, _window->getHeight() / 2);
+	setCameraControll(_isCameraControll);
 }
 
 
@@ -441,10 +443,7 @@ void MainGameScene::fixedStepUpdate(double deltaTime)
 
 	_sceneManager->getBusStopSystem()->update(deltaTime, _activeBus);
 
-	if (_isCameraControll)
-	{
-		_sceneManager->getGameLogicSystem()->update(deltaTime);
-	}
+	_sceneManager->getGameLogicSystem()->update(deltaTime);
 }
 
 
@@ -764,10 +763,7 @@ void MainGameScene::fixedStepReadInput(float deltaTime)
 	// mouse
 	if (input.isMouseButtonPressed(GLFW_MOUSE_BUTTON_RIGHT))
 	{
-		_isCameraControll = !_isCameraControll;
-		glfwSetCursorPos(_window->getWindow(), _window->getWidth() / 2, _window->getHeight() / 2);
-
-		_window->setCursorMode(_isCameraControll ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL);
+		setCameraControll(!_isCameraControll);
 	}
 
 	if (input.isMouseButtonReleased(GLFW_MOUSE_BUTTON_LEFT))
@@ -824,4 +820,20 @@ void MainGameScene::rayTestWithModelNode(RenderObject* renderObject, ModelNode* 
 	{
 		rayTestWithModelNode(renderObject, modelNode->getChildren()[i], rayStart, rayDir, modelMatrix);
 	}
+}
+
+
+void MainGameScene::setCameraControll(bool isCameraControll)
+{
+	_isCameraControll = isCameraControll;
+
+	Component* cameraControlComponent = _activeCamera->getSceneObject()->getComponent(CT_CAMERA_CONTROL);
+	if (cameraControlComponent != nullptr)
+	{
+		static_cast<CameraControlComponent*>(cameraControlComponent)->setRotationControl(_isCameraControll);
+	}
+
+	glfwSetCursorPos(_window->getWindow(), _window->getWidth() / 2, _window->getHeight() / 2);
+
+	_window->setCursorMode(_isCameraControll ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL);
 }
